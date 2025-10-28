84532261c6b3f5436fed947aabb0b7207320ee14
Raunak Singh <120004875+RaunakK22UB@users.noreply.github.com>
2025-10-28 13:08:45 +0530

    api_prec_changes

--- api-call-prec/main.js
+++ api-call-prec/main.js
@@ -1,6 +1,19 @@
 const   lists = document.querySelector('.List-data')
 
 
+lists.addEventListener('click', function(event) {
+    
+    // Check if the element that was clicked has the class 'delete-btn'
+    if (event.target.classList.contains('delete-btn')) {
+        
+        // Find the closest parent div with the class 'post'
+        const postDiv = event.target.closest('.post');
+        
+        // Remove that div from the page
+        postDiv.remove();
+    }
+});
+
 
 
 async function fetchFunction(){
@@ -20,6 +33,7 @@ function displaypost(posts){
                 <div class="post">
                 <p>${index}. ${post.title}</p>
                 <p>content: ${post.body}</p>
+                <button class="delete-btn" > delete</button>
                 </div>
 
               `
